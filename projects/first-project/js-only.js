c3df1e43301fcb59a083379a17a5169e5e359025
rgoshen <rick.goshen@gmail.com>
2022-10-13 19:42:20 -0700

    add first ts project as example

---
+++ projects/first-project/js-only.js
@@ -0,0 +1,15 @@
+// const button = document.querySelector('button');
+// const input1 = document.getElementById('num1'); // string
+// const input2 = document.getElementById('num2'); // string
+
+// const add = (num1, num2) => {
+//   if (num1 === 'number' && num2 === 'number') {
+//     return num1 + num2;
+//   } else {
+//     return +num1 + +num2; // convert to number
+//   }
+// };
+
+// button.addEventListener('click', () =>
+//   console.log(add(input1.value, input2.value))
+// );
